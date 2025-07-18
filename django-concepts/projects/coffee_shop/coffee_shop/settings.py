319f00ba1b5a718370dc91a9d7ddc9b56eb18283
Yesica Giraldo <yagiraldot@ieee.org>
2025-07-18 18:27:15 -0500

    Ejercicio completo con usuario, vistas, logeo en django

--- django-concepts/projects/coffee_shop/coffee_shop/settings.py
+++ django-concepts/projects/coffee_shop/coffee_shop/settings.py
@@ -38,7 +38,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'crispy_forms',
+    'crispy_tailwind',
     'products',
+    'users',
+
 ]
 
 MIDDLEWARE = [
@@ -122,3 +126,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+CRISPY_ALLOWED_TEMPLATE_PACKS = "tailwind"
+
+CRISPY_TEMPLATE_PACK = "tailwind"
+LOGIN_REDIRECT_URL = "list_product"
