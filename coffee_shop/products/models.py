410062913f13b1720a821712b40b2857a798c88c
Yesica Giraldo <yagiraldot@ieee.org>
2025-07-31 23:13:29 -0500

    Subiendo avances de mi curso de django + base AWS

---
+++ coffee_shop/products/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+
+# Create your models here.
+class Product(models.Model):
+    name = models.TextField(max_length=250, verbose_name="nombre")
+    description = models.TextField(max_length=300, verbose_name="descripción")
+    price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name="precio")
+    available = models.BooleanField(default=True, verbose_name="Disponible")
+    photo = models.ImageField(upload_to="Logos", null = True, blank=True, verbose_name="foto")
+    
+
+    def __str__(self):
+        return self.name
---
+++ coffee_shop/products/urls.py
@@ -0,0 +1,24 @@
+"""
+URL configuration for coffee_shop project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/5.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.urls import path
+from .views import ProductFormView, ProductListView, ProductListAPI
+
+urlpatterns = [
+    path('', ProductListView.as_view(), name='list_product'),
+    path('api/', ProductListAPI.as_view(), name='list_product_api'),
+    path('agregar/', ProductFormView.as_view(), name = "add_product"),
+]
